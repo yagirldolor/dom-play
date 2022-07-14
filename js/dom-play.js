24054cdbe93c5dab76aa17dad4f66d513a7cf924
yagirldolor <deusebio@sea.yearup.org>
2022-07-14 11:39:18 -0700

    updates forked files

---
+++ js/dom-play.js
@@ -0,0 +1,12 @@
+//alert("Hello, World");
+
+
+let actors = document.querySelectorAll(".moana");
+
+console.log(actors);
+
+for (let i = 0; i < actors.length; i++) {
+    actors[i].style.backgroundColor = "yellow";
+   }
+
+document.querySelector(".moana").style.backgroundColor = "yellow";

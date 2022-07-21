ed40d2004a83186b95d37e28e4aa4f09b7561302
yagirldolor <deusebio@sea.yearup.org>
2022-07-21 14:15:13 -0700

    almost done

--- js/dom-play.js
+++ js/dom-play.js
@@ -1,12 +1,60 @@
 //alert("Hello, World");
 
 
-let actors = document.querySelectorAll(".moana");
+// let actors = document.querySelectorAll(".moana");
 
-console.log(actors);
+// console.log(actors);
 
-for (let i = 0; i < actors.length; i++) {
-    actors[i].style.backgroundColor = "yellow";
-   }
+// for (let i = 0; i < actors.length; i++) {
+//     actors[i].style.backgroundColor = "yellow";
+//    }
 
-document.querySelector(".moana").style.backgroundColor = "yellow";
+// document.querySelector(".moana").style.backgroundColor = "yellow";
+
+function myAlert(){
+    alert("I'm clicked");
+ }
+
+let spans = document.querySelectorAll("#play span");
+
+console.log(spans);
+
+for(const mySpan of spans){
+    mySpan.addEventListener("click", function(ev){
+        highlightActor(mySpan.dataset.actor); //hardwired 'actor'
+    }); //myAlert is an identity of the function above
+    //alert(mySpan.dataset.actor);
+}
+
+function highlightActor(actor){
+    for(const mySpan of spans){
+        if(actor == mySpan.dataset.actor){//current actor, highlight
+            mySpan.style.backgroundColor = 'pink';
+            mySpan.style.color = 'white';
+            mySpan.style.fontWeight = 'bolder'
+            mySpan.style.fontSize = '20px';
+        }else{//not current actor, remove highlight
+            mySpan.style.backgroundColor = 'white';
+            mySpan.style.color = 'black';
+            mySpan.style.fontWeight = 'normal'
+            mySpan.style.fontSize = '13px';
+        }
+
+    }
+}
+
+//toggle function
+function highlight(el){
+
+    if(el.style.backgroundColor == "pink"){
+        el.style.backgroundColor='white';
+    }else{
+        el.style.backgroundColor='pink';
+    }
+    
+ }
+
+
+ 
+
+ //#594100
